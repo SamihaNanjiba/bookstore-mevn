ab6dc4488b530cf6953557b4279952977a3258db
Samiha Nanjiba <snanjiba@gmail.com>
2024-03-27 13:00:30 +0100

    add tailwind css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+  <header class="bg-red-400 text-white">
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      HelloWorld!
     </div>
   </header>
 
   <main>
-    <TheWelcome />
   </main>
 </template>
 
